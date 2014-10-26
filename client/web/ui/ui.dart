7ac5d4007cba1dad87042d380efd3f8aef06219f
Pierre Falez <pierre.falez@gmail.com>
2014-10-26 18:44:45 +0100

    Add test comp

--- client/web/ui/ui.dart
+++ client/web/ui/ui.dart
@@ -1,4 +1,14 @@
 library ui;
 
+/*
+ * Component
+ */
 part 'component/component.dart';
 part 'component/container.dart';
+part 'component/bidon.dart';
+
+/*
+ * Style
+ */
+
+part 'style/bidon_style.dart';

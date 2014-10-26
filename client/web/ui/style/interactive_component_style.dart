a4c5ee8cbf359669614fb3e4539f0bfe76b3f09b
Nicolas <Phoenix-Left@hotmail.fr>
2014-10-26 18:35:30 +0100

    Add styles classes

---
+++ client/web/ui/style/interactive_component_style.dart
@@ -0,0 +1,10 @@
+part of ui;
+
+/* A Style of a component */
+abstract class InteractiveComponentStyle extends ComponentStyle {
+
+	/* Constructors */
+	
+	InteractiveComponentStyle() {}
+
+}
--- client/web/ui/ui.dart
+++ client/web/ui/ui.dart
@@ -2,3 +2,5 @@ library ui;
 
 part 'component/component.dart';
 part 'component/container.dart';
+part 'style/component_style.dart';
+part 'style/interactive_component_style.dart';

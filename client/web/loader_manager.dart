cb9b9b92a9e9d6a6e21648a8ef60e6b3daf39247
Antoine PHILIPPE <antoine.philippe62@gmail.com>
2014-10-27 20:47:14 +0100

    Added ObjectImage => container for ImageElement + width & height parameters

--- client/web/loader_manager.dart
+++ client/web/loader_manager.dart
@@ -1,29 +1,55 @@
 part of ressource_manager;
 
+final _SPRITES_FOLDER = 'sprites/';
+
 class LoaderManager{
 	var _values;
-	Map<String, ImageElement> _map;
+	Map<String, ImageObject> _map;
 	ImageElement _pic;
 	
+	
 	LoaderManager(){
-		_map = new Map<String, ImageElement>();
+		_map = new Map<String, ImageObject>();
 		_pic = new ImageElement();
 		HttpRequest.getString('sprites.json')
 		.then(processString);
 	}
 	
-	 processString(String stringName){
-		_values = JSON.decode(stringName);
-		_values.forEach((k,v) => print(v.toString()));
-		_values.forEach((k,v) => _pic.src = v.toString());
-		_values.forEach((k,v) => _map[k.toString()] = _pic);
+	 processString(String fileContent){
+		print(fileContent.toString());
+		_values = JSON.decode(fileContent);
+		print(_values.forEach());
+		_values.forEach((k,v) => _map[k.toString()] = new ImageObject(50, 50,_SPRITES_FOLDER + v.toString()));
 	}
 	 
-	ImageElement getImage(String imageName){
+	ImageObject getImage(String imageName){
 		return _map[imageName];
 	}
 	
+}
+
+class ImageObject{
 	
+	int _width;
+	int _height;
+	ImageElement _img;
 	
+	ImageObject(int width, int height, String src){
+		this._width = width;
+		this._height = height;
+		this._img = new ImageElement(src: src);
+	}
+	
+	int getWidth(){
+		return this._width;
+	}
+	
+	int getHeight(){
+		return this._height;
+	}
+	
+	ImageElement getImage(){
+		return this._img;
+	}
 	
 }

4fef8180c633078b3782eeab9207931b98f95323
Antoine PHILIPPE <antoine.philippe62@gmail.com>
2014-10-27 20:05:35 +0100

    Fixed : loader_manager re-implemented (NOOB inside)

--- client/web/loader_manager.dart
+++ client/web/loader_manager.dart
@@ -1,2 +1,29 @@
 part of ressource_manager;
 
+class LoaderManager{
+	var _values;
+	Map<String, ImageElement> _map;
+	ImageElement _pic;
+	
+	LoaderManager(){
+		_map = new Map<String, ImageElement>();
+		_pic = new ImageElement();
+		HttpRequest.getString('sprites.json')
+		.then(processString);
+	}
+	
+	 processString(String stringName){
+		_values = JSON.decode(stringName);
+		_values.forEach((k,v) => print(v.toString()));
+		_values.forEach((k,v) => _pic.src = v.toString());
+		_values.forEach((k,v) => _map[k.toString()] = _pic);
+	}
+	 
+	ImageElement getImage(String imageName){
+		return _map[imageName];
+	}
+	
+	
+	
+	
+}

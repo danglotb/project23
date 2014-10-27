0a26e699187454af176005ee375aa72759e62263
Antoine PHILIPPE <antoine.philippe62@gmail.com>
2014-10-27 14:39:43 +0100

    re : add ressource manager (my bad!)

---
+++ client/web/game/ressource_manager.dart
@@ -0,0 +1,54 @@
+part of game;
+
+class LoaderManager{
+	
+	LoaderManager(){
+		var dir = new io.Directory('./web/game/sprites');
+		core.List<html.ImageElement> liste = new core.List<html.ImageElement>();
+		html.ImageElement pic;
+		dir.list(recursive: true, followLinks: false)
+			.listen((io.FileSystemEntity entity){
+			pic = new html.ImageElement();
+			pic.src = entity.path;
+			liste.add(pic);
+			core.print(entity.path);
+		});
+		}
+}
+
+
+class SpriteManager{
+	
+	
+	
+}
+//
+//class RessourceManager{
+//	final tile_size = 50;
+//	final tileset_width = 200;
+//	final tilset_height = 300;
+//	
+//	List<int> tileset;
+//	ImageElement pic;
+//	
+//	RessourceManager(){
+//			this.pic = new ImageElement();
+//			pic.src = './Tiles.png';
+//			querySelector("p").append(pic);
+//			
+//	}
+//	
+//	
+//	String toString(){
+//		return "toto";
+//	}
+//	
+//}
+
+
+
+void main(){
+	//RessourceManager rm = new RessourceManager();
+	LoaderManager lm = new LoaderManager();
+	
+}

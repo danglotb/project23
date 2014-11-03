f411418df5112798629aeb00c9032b40fd04c317
Benjamin DANGLOT <danglotb@acajou26.iut-infobio.priv.univ-lille1.fr>
2014-11-03 12:57:07 +0100

    update gameboard

--- client/web/game/gameboard.dart
+++ client/web/game/gameboard.dart
@@ -1,5 +1,7 @@
 part of game;
 
+final int SIZE_CASE = 50;
+
 class Gameboard {
 	
 	List<Case> _gameboardTab;
@@ -13,7 +15,7 @@ class Gameboard {
 	Player player;
 	
 	Gameboard(utils.Vector2D dimensions , html.CanvasElement canvas, {List<Case> gameCase}) {
-		this._camera = new Camera(new utils.Vector2D(10,10), new utils.Coordinates2D(0,0));
+		this._camera = new Camera(new utils.Vector2D(10*SIZE_CASE ,10*SIZE_CASE ), new utils.Coordinates2D(0,0));
 		//TO REMOVE
 		this.canvas = canvas;
 	  this.canvas.width =  html.window.innerWidth;
@@ -71,6 +73,8 @@ class Gameboard {
 	}
 	
 	void draw(num timer) {
+	  
+	  
 		for (int y = this._camera.getCoordinates().y ; y < this._camera.getDimensions().y ; y++) {
       			for (int x = this._camera.getCoordinates().x ; x < this._camera.getDimensions().y ; x++) {
       				if (this._gameboardTab[x+(y*this._camera.getDimensions().y)].getSpriteValue() == 1)
@@ -79,7 +83,6 @@ class Gameboard {
       					canvas.context2D.drawImageScaled(this.sand, x*50, y*50, 50,50);
       			}
       		}
-		
 		canvas.context2D.drawImageScaled(this.player.getSkin(), this.player.getCoordinates2D().x, 
 		this.player.getCoordinates2D().y, 50 ,50);
  			if (this.player.onMove)

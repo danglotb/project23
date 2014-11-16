949657e0bdb253bf0ff0f4d4f66bf74d01e55ac8
bdanglot <bdanglot@gmail.com>
2014-11-16 18:28:27 +0100

    add gitignore

--- client/web/client.dart
+++ client/web/client.dart
@@ -1,5 +1,20 @@
-import 'dart:html';
+import 'dart:html' as html;
+import 'game/game.dart'; 
+import 'utils/utils.dart' as utils;
+
+final int size = 50;
+final int SIZE_CASE = 50;
 
 void main() {
-	print('test');
+	html.CanvasElement canvas = html.querySelector("#main-frame");
+	List<Case> cases = new List<Case>(size*size);
+	for (int y = 0 ; y < size ; y++) {
+  			for (int x = 0 ; x < size; x++) {
+					if (x % 10 == 0 || y % 10 == 0)
+						cases[x+(y*size)] = new Case(new utils.Coordinates2D(x,y) , spriteValue:0);
+					else
+						cases[x+(y*size)] = new Case(new utils.Coordinates2D(x,y) , spriteValue:1);
+  			}
+	}
+  Gameboard gb = new Gameboard(new utils.Vector2D(SIZE_CASE*size,SIZE_CASE*size),canvas, gameCase:cases);
 }
